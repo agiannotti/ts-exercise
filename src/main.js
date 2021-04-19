50eb304008a5ce63850d812e557a2c8b6ec50b49
tonyg <aj.gianno@gmail.com>
2021-04-18 20:56:11 -0500

    count component

---
+++ src/main.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var app_module_1 = require("./app/app.module");
+var environment_1 = require("./environments/environment");
+var like_component_1 = require("./like.component");
+if (environment_1.environment.production) {
+    core_1.enableProdMode();
+}
+var component = new like_component_1.LikeComponent(10, true);
+component.onClick();
+console.log("likesCount: " + component.likesCount + ", isSelected: " + component.isSelected);
+platform_browser_dynamic_1.platformBrowserDynamic()
+    .bootstrapModule(app_module_1.AppModule)
+    .catch(function (err) { return console.error(err); });
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { LikeComponent } from './like.component';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let component = new LikeComponent(10, true);
+component.onClick();
+console.log(
+  `likesCount: ${component.likesCount}, isSelected: ${component.isSelected}`
+);
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
