d78bcaba666a8533ea7c0ac60c18b6e97f7154bc
tonyg <aj.gianno@gmail.com>
2021-04-19 13:33:27 -0500

    property binding attr if not found in DOM

--- src/app/author-list/author-list.component.ts
+++ src/app/author-list/author-list.component.ts
@@ -1,13 +1,16 @@
 import { AuthorsService } from './../authors.service';
 import { Component } from '@angular/core';
 
+//property binding attr if html property not found in the DOM
 @Component({
   selector: 'author-list',
   templateUrl: './author-list.component.html',
   styleUrls: ['./author-list.component.css'],
 })
 export class AuthorListComponent {
+  imageUrl = 'https://loremflickr.com/320/240';
   title = ' Authors';
+  colSpan = 2;
   count: Number;
   authors;
   constructor(service: AuthorsService) {
