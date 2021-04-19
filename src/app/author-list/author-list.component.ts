f5a630d709d4dd96452eb266524fb12d8504aeeb
tonyg <aj.gianno@gmail.com>
2021-04-19 13:03:41 -0500

    count component

--- src/app/author-list/author-list.component.ts
+++ src/app/author-list/author-list.component.ts
@@ -2,14 +2,16 @@ import { AuthorsService } from './../authors.service';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-author-list',
+  selector: 'author-list',
   templateUrl: './author-list.component.html',
   styleUrls: ['./author-list.component.css'],
 })
 export class AuthorListComponent {
-  title = 'Author List';
+  title = ' Authors';
+  count: Number;
   authors;
   constructor(service: AuthorsService) {
     this.authors = service.getAuthors();
+    this.count = service.getCount();
   }
 }
--- src/app/authors.service.ts
+++ src/app/authors.service.ts
@@ -2,4 +2,8 @@ export class AuthorsService {
   getAuthors() {
     return ['Jack London', 'Cormac McCarthy', 'Jane Austen'];
   }
+
+  getCount() {
+    return this.getAuthors().length;
+  }
 }
